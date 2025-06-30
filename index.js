e13264cad5ebe5fe16b21d8f9984a20fc5b8cc48
Mahiru Shiina <halovemusics@gmail.com>
2025-06-30 13:42:46 +0700

    Cài CORS

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const database = require("./config/database");
 const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
+const cors = require("cors"); 
 database.connect();
 // Middleware để đọc body từ client, không cần body-parser nâng cao
 app.use(express.json()); // Đọc JSON từ client (axios/fetch gửi lên)
 app.use(express.urlencoded({ extended: true })); // Nếu dùng form HTML gửi lên
-
+//  Cho phép CORS
+app.use(cors()); // 👈 cấu hình mặc định: cho phép tất cả origin
 const routesApiV1 = require("./api/v1/routes/index.route");
 routesApiV1(app);
 
