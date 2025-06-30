c07c995aab8542f30c9c489788b20b9b35518e0d
Mahiru Shiina <halovemusics@gmail.com>
2025-06-30 14:32:05 +0700

    Tính năng đăng nhập

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -23,3 +23,22 @@ module.exports.register = async (req, res) => {
         res.status(500).json({ message: "Lỗi server" });
     }
 };
+
+//[POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email, deleted: false });
+        if (!user) {
+            return res.status(400).json({ message: "Tài khoản không tồn tại" });
+        }
+        if (user.password !== md5(req.body.password)) {
+            return res.status(400).json({ message: "Sai mật khẩu" });
+        }
+        const token = user.token;
+        res.cookie("token", token, { httpOnly: true });
+        res.json({ code: 200, message: "Login thành công", token: token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Lỗi server" });
+    }
+};
