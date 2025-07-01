5dac692777a2f078beaa3b93dc2c49f54bffd11a
Mahiru Shiina <halovemusics@gmail.com>
2025-07-01 14:27:37 +0700

    Lưu id user đã tạo task

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -98,6 +98,7 @@ module.exports.changeMulti = async (req, res) => {
 //[POST] /api/v1/tasks/create
 module.exports.create = async (req, res) => {
 try {
+  req.body.createdBy = req.user._id;
     const newTask = new Task(req.body);
     await newTask.save();
     res.json(newTask);
